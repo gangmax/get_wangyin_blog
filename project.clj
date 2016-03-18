12fba7ab8d377335e19112d786c7768642a2f4a1
Max Huang <gangmax@gmail.com>
2016-03-18 16:52:15 +0800

    Update the Clojure/enlive version to the latest. And, in the latest minutes, Yin updated the latest post "Google Vision".

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/gangmax"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [enlive "1.1.5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
                 ]
   :jvm-opts ^:replace [] ;Fix the "TieredCompilation is disabled in this release" error, from "https://github.com/technomancy/leiningen/wiki/Faster"
   :main get_wangyin_blog.core)
