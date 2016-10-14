7724fedba08a90d9ea11b011f3a26a47c24223f4
Max Huang <gangmax@gmail.com>
2016-10-14 19:31:06 +0800

    Add the support to trim the html prefix/postfix of the markdown content.

--- src/get_wangyin_blog/core.clj
+++ src/get_wangyin_blog/core.clj
@@ -43,6 +43,21 @@
           ".markdown")
         links))
     
+    (defn trim-content
+      "Get the meaningful text content from the HTML content."
+      [content prefix postfix]
+      (let [
+        cannot-trim-tag "CANNOT find trim prefix/postfix!!!!!!\n\n\n"
+        prefix-index (string/index-of content prefix)
+        postfix-index (string/index-of content postfix)]
+        (if (and 
+              (not (nil? prefix-index))
+              (not (nil? postfix-index))
+              (>= prefix-index 0)
+              (> postfix-index prefix-index))
+          (subs content (+ prefix-index (count prefix)) postfix-index)
+          content)))
+
     (pmap
       #(let [
           file-path-name (str target-directory (second %))
@@ -52,10 +67,15 @@
           url (let [link (first %)]
                 (if (= "www." (subs (second (string/split link #"//")) 0 4))
                   link
-                  (str (first (string/split link #"//")) "//www." (second (string/split link #"//")))))]
+                  (str (first (string/split link #"//")) "//www." (second (string/split link #"//")))))
+          prefix "<div style=\"margin: 2% 5% 2% 5%\">\n\n<table>\n\n<tbody>\n\n<tr>\n\n<td width=\"60%\">\n\n<div style=\"padding: 2% 8% 5% 8%; border: 1px solid LightGrey;\">\n\n"
+          postfix "\n</div>\n\n<div style=\"margin-top: 5px\"><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></div>\n\n</td>\n\n<td width=\"16%\" valign=\"top\"><script>(adsbygoogle = window.adsbygoogle || []).push({});</script></td>\n\n</tr>\n\n</tbody>\n\n</table>\n\n</div>\n"]
         (do
           (println (str "Downloading '" url "'..."))
-          (spit file-path-name (:out (sh (str (System/getProperty "user.dir") "/resources/h2m.js") url)))
+          (spit file-path-name 
+            (trim-content 
+              (:out (sh (str (System/getProperty "user.dir") "/resources/h2m.js") url))
+              prefix postfix))
           (println (str "Saved to " file-path-name "..."))
           file-path-name))
       (zipmap links file-names))))
