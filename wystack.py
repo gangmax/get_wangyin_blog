1411382049d6924511e7ee76c557723a48400175
Max Huang <gangmax@gmail.com>
2023-01-17 19:11:46 +0800

    Add code to handle the image tag.

--- wystack.py
+++ wystack.py
@@ -13,10 +13,10 @@
 
 import json
 import os
-import requests
+import re
 import sys
+import requests
 from slugify import slugify
-from urllib.parse import quote, unquote
 
 INDEX_URL = 'https://yinwang1.substack.com/api/v1/archive?sort=new&limit=12&offset=0'
 INDEX_DATE_TITLE_REGEX = r'(\d{4})/(\d{2})/(\d{2})/(.*)/$'
@@ -43,6 +43,9 @@ def parse_index(index_url):
             'date': date})
     return result
 
+def get_filename(item):
+    return 'ss-{}-{}.markdown'.format(item['date'], slugify(item['title']))
+
 def parse_page_to_post(page_url, start_tag = PAGE_CONTENT_START_TAG,
     end_tag = PAGE_CONTENT_END_TAG):
     '''
@@ -52,12 +55,34 @@ def parse_page_to_post(page_url, start_tag = PAGE_CONTENT_START_TAG,
         page_url, start_tag, end_tag))
     return stream.read()
 
+def convert_image_tag(content: str) -> str:
+    m = re.search(r'\!\[\]\((.+)\)\<\/picture\>', content)
+    return '![]({})'.format(m.group(1))
+
+CONVERTING_FUNCTION_MAP = {
+    '<div cla': lambda x: "",
+    '<figure>': convert_image_tag,
+    '</div>': lambda x: "",
+}
+
+def optimize_content(raw: str) -> str:
+    '''
+    Optimize the text content by:
+        1. Remove the useless html tags such as "<div>" and "</div>".
+        2. Convert the original image tag to readable content.
+    '''
+    result = ''
+    for line in raw.splitlines():
+        if line[0:8] in CONVERTING_FUNCTION_MAP.keys():
+            result = result + CONVERTING_FUNCTION_MAP[line[0:8]](line) + '\n'
+        else:
+            result = result + line + '\n'
+    return result
+
 def write_post(content, target_filename, target_dir):
     with open('{}/{}'.format(target_dir, target_filename), 'w') as f:
         f.write(content)
 
-def get_filename(item):
-    return 'ss-{}-{}.markdown'.format(item['date'], slugify(item['title']))
 
 if __name__ == '__main__':
     given_url = sys.argv[1] if len(sys.argv) > 1 else INDEX_URL
@@ -66,7 +91,8 @@ if __name__ == '__main__':
     for item in items:
         target_dir = BASE_BLOG_PATH
         filename = get_filename(item)
-        content = parse_page_to_post(item['url'])
-        write_post(content, filename, target_dir)
+        raw_content = parse_page_to_post(item['url'])
+        optimize_content = optimize_content(raw_content)
+        write_post(optimize_content, filename, target_dir)
         print('Finish getting the "{}/{}" post...'.format(target_dir, filename))
     print('Done.')
